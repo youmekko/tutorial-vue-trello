2c79ea994a69dcc7d677b63ae51d1a3ab1a2f3d0
Choi Youme <ymchoi@wemb.co.kr>
2019-01-10 15:31:37 +0900

    vue-router 사용해보기

--- src/main.js
+++ src/main.js
@@ -1,22 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-const Login = { template: '<div>Login Page</div>' }
+//미들웨어
+Vue.use(VueRouter)
 
-const routes  = {
-  '/' : App,
-  '/login' : Login
-}
+const Login = {template: '<div>Login Page</div>'}
+const NotFound = {template: '<div>Page not found</div>'}
+
+
+const router = new VueRouter({
+  /*
+  브라우저에서 라우팅 할때는 해쉬뱅(Hashbang)모드라는게 동작하는데 (브라우저 히스토리 API가 없을 때 사용)
+  크롬의 경우는 history API가 있기 때문에 해시뱅 모드가 아닌 히스토리 모드를 사용하면 된다.
+  */
+  mode: 'history',
+  routes: [
+    {path: '/', component: App},
+    {path: '/login', component: Login},
+    {path: '*', component: NotFound}
+  ]
+})
+
 
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent(){
-      return routes[window.location.pathname] ||
-        { template : '<div>Page not found</div>'}
-    }
-  },
-  render(h){
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })
