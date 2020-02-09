71d58389ad3214660614422b4c156a1a2cf73364
youmekko <youmekko.o@gmail.com>
2020-02-09 11:02:22 +0900

    vuex로 옮긴 인증 로직 적용하기

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -21,6 +21,8 @@
 
 <script>
   import {auth, setAuthInHeader} from '../api'
+  import { mapActions } from 'vuex'
+
   export default {
     data() {
       return {
@@ -39,11 +41,12 @@
       this.rPath = this.$route.query.rPath || '/'
     },
     methods: {
+      ...mapActions([
+          'LOGIN'
+      ]),
       onSubmit() {
-        auth.login(this.email, this.password)
+        this.LOGIN({email: this.email, password: this.password})
           .then(data => {
-            localStorage.setItem('token', data.accessToken)
-            setAuthInHeader(data.accessToken)
             this.$router.push(this.rPath)
           })
           .catch(err => {
