faeb4a2b3570cb3ca2f73368b23681a986f6d729
youmekko <youmekko.o@gmail.com>
2020-02-09 13:59:32 +0900

    보드 생성 후 바로 보드화면으로 이동하기 구현

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import * as api from '../api'
 
 const actions = {
     ADD_BOARD(_, {title}){
-        return api.board.create(title)
+        return api.board.create(title).then(data => data.item)
     },
     /* actions의 첫번째 인자로는 컨텍스트 객체가 오는데 컨텍스트 객체 중에 commit 이라는 함수를 쓴다.
     내부적으로 SET_BOARDS를 이용해 boards 상태를 갱신하도록 한다. */
