3d4a583740b7d18f9603aa183ab29a91b3cb7aa5
youmekko <youmekko.o@gmail.com>
2020-06-05 11:16:39 +0900

    카드 상세 조회 화면 완료

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,18 @@
 <template>
-  <Modal>
-    <div slot="body">
-      {{ card }}
+  <Modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" readonly>
+       </div>
+       <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        readonly
+        v-modle="card.description"></textarea>
+    </div>
+    <div slot="footer"></div>
   </Modal>
 </template>
 
@@ -24,11 +34,35 @@
     methods: {
       ...mapActions([
         'FETCH_CARD'
-      ])
+      ]),
+      onClose() {
+        this.$router.push(`/b/${this.board.id}`)
+      }
     }
   }
 </script>
 
 <style scoped>
+.modal-card .modal-container {
+  min-width: 300px;
+  min-height: 800px;
+  width: 68%
+}
+
+.modal-card-header-title {
+  padding-right: 30px;
+}
+
+.modal-close-btn {
+  position: absolute;
+  top: 8px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+
+.modal-card-header {
+  position: relative;
+}
 
 </style>
