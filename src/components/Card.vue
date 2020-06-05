ab40f8c7ef9d950ac180db941f4b421b6e069f3e
youmekko <youmekko.o@gmail.com>
2020-06-05 16:04:47 +0900

    카드 수정 기능 완료

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,15 +2,16 @@
   <Modal class="modal-card">
     <div slot="header" class="modal-card-header">
       <div class="modal-card-header-title">
-        <input class="form-control" type="text" :value="card.title" readonly>
+        <input class="form-control" type="text" :value="card.title" @click="toggleTitle=true" @blur="onBlurTitle" :readonly="!toggleTitle"
+        ref="inputTitle">
        </div>
        <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
     <div slot="body">
       <h3>Description</h3>
       <textarea class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
-        readonly
-        v-modle="card.description"></textarea>
+        :readonly="!toggleDescription" @click="toggleDescription=true" @blur="onBlurDescription" ref="inputDescription"
+        v-model="card.description"></textarea>
     </div>
     <div slot="footer"></div>
   </Modal>
@@ -22,21 +23,46 @@
 
   export default {
     components: { Modal },
+    data() {
+      return {
+        toggleTitle: false,
+        toggleDescription: false
+      }
+    },
     computed: {
       ...mapState({
+        board: 'board',
         card: 'card'
       })
     },
     created() {
-      const id = this.$route.params.cid
-      this.FETCH_CARD({id})
+      this.fetchCard()
     },
     methods: {
       ...mapActions([
-        'FETCH_CARD'
+        'FETCH_CARD',
+        'UPDATE_CARD'
       ]),
+      fetchCard() {
+           const id = this.$route.params.cid
+           this.FETCH_CARD({id})
+      },
       onClose() {
         this.$router.push(`/b/${this.board.id}`)
+      },
+      onBlurTitle() {
+        this.toggleTitle = false
+        const title = this.$refs.inputTitle.value.trim()
+        if (!title) return
+        this.UPDATE_CARD({id: this.card.id, title})
+          .then(() => this.fetchCard())
+      },
+      onBlurDescription() {
+        this.toggleDescription = false
+        const description = this.$refs.inputDescription.value.trim();
+         if (!description) return
+        this.UPDATE_CARD({id: this.card.id, description})
+          .then(() => this.fetchCard())
       }
     }
   }
--- src/store/actions.js
+++ src/store/actions.js
@@ -30,8 +30,13 @@ const actions = {
         return api.card.fetch(id).then(data => {
             commit('SET_CARD', data.item)
         })
-    }
+    },
 
+    UPDATE_CARD({dispatch, state}, {id, title, description, pos, listId}) {
+        return api.card.update(id, {title, description, pos, listId}).then(() => {
+            dispatch('FETCH_BOARD', {id: state.board.id})
+        })
+    }
 }
 
 export default actions
